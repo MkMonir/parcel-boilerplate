d852aca527570eeb9d20e1e6ef43c0c357ab69ee
MkMonir <khanmonir1335@gmail.com>
2022-01-30 19:42:23 +0600

    Parcel Boilerplate

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./css/fromjs.sass";
+
+// I know this is dumb
+// Just for demo purpose
+async function asyncAwaitSample() {
+  const { tes } = await import("./tes");
+  const result = await tes();
+  console.log(result);
+}
+
+asyncAwaitSample();
+
+const mail = "khanmonir@gmail.com";
